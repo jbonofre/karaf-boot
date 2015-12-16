8eaac5d5c05e61a6e92c5293d12743cabf85a8ef
Christian Schneider <chris@die-schneider.net>
2015-12-16 16:00:05 +0100

    Added repository attr

--- karaf-boot-samples/karaf-boot-sample-tasklist/src/main/java/org/apache/aries/jpa/example/tasklist/feature/Feature.java
+++ karaf-boot-samples/karaf-boot-sample-tasklist/src/main/java/org/apache/aries/jpa/example/tasklist/feature/Feature.java
@@ -3,6 +3,7 @@ package org.apache.aries.jpa.example.tasklist.feature;
 public @interface Feature {
     String name();
     String version() default "";
+    String[] repositories() default {};
     String[] features();
     String pid() default "";
     String[] config() default {};
